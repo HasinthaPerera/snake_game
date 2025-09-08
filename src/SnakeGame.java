2f74f2757b71c17a4364f4bb6e72af7fe79ba209
Asintha-Banuka <asinthawanasinha653@gmail.com>
2025-09-08 21:09:10 +0530

    Reduce speed

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -21,7 +21,7 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
         setBackground(Color.BLACK);
         initGame();
 
-        timer = new Timer(100, this);
+        timer = new Timer(150, this);
         timer.start();
         setFocusable(true);
         addKeyListener(this);
